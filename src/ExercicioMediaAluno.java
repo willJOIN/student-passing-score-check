fe9ad994ae56457ec1b60ffabf8844b9b766e9f9
willJOIN <whirano4@gmail.com>
2022-09-30 21:24:28 -0300

    style: Arruma comentário

--- src/ExercicioMediaAluno.java
+++ src/ExercicioMediaAluno.java
@@ -2,18 +2,21 @@ import java.util.Scanner;
 
 public class ExercicioMediaAluno {
     public static void main(String[] args) {
-//        Faça um programa que lê as duas notas parciais obtidas por um aluno numa disciplina ao longo de um semestre, e calcule a sua média.
-//        A atribuição de conceitos obedece à tabela abaixo:
+        /*
+            Faça um programa que lê as duas notas parciais obtidas por um aluno numa disciplina ao longo de um semestre, e calcule a sua média.
+            A atribuição de conceitos obedece à tabela abaixo:
 
-//        Média de Aproveitamento  Conceito
-//        Entre 9.0 e 10.0        A
-//        Entre 7.5 e 9.0         B
-//        Entre 6.0 e 7.5         C
-//        Entre 4.0 e 6.0         D
-//        Entre 4.0 e zero        E
+            Média de Aproveitamento  Conceito
+            Entre 9.0 e 10.0        A
+            Entre 7.5 e 9.0         B
+            Entre 6.0 e 7.5         C
+            Entre 4.0 e 6.0         D
+            Entre 4.0 e zero        E
 
-//        O algoritmo deve mostrar na tela as notas, a média,
-//        o conceito correspondente e a mensagem “APROVADO” se o conceito for A, B ou C ou “REPROVADO” se o conceito for D ou E.
+            O algoritmo deve mostrar na tela as notas, a média, o conceito correspondente
+            e a mensagem “APROVADO” se o conceito for A, B ou C ou “REPROVADO” se o conceito for D ou E.
+         */
+
 
         float nota1 = 0, nota2 = 0, media = 0;
 
